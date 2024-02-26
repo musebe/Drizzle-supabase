8a26ae63f14cb3dd57ae3a73f3d139330c3543ea
musebe <emusebe9@gmail.com>
2024-02-26 15:28:29 +0300

    added next- image to demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'loremflickr.com' },
+      { hostname: 'picsum.photos' },
+    ],
+  },
+};
 
 export default nextConfig;
